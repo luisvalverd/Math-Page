ce3699be5b2a41fdf502b2527dd31b3c3820ec09
luisvalverde120 <luis.valverde120@gmail.com>
2023-06-15 12:20:46 -0500

    navbar responsive

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,5 @@
-import { Navbar, Button } from "flowbite-react";
-import type { CustomFlowbiteTheme } from "flowbite-react";
+import { Navbar } from "flowbite-react";
 import Link from "next/link";
-
-const customNavbar: CustomFlowbiteTheme["navbar"] = {};
 
 export default function NavbarPage() {
   return (
@@ -16,7 +13,7 @@ export default function NavbarPage() {
         </Link>
       </div>
 
-      <Navbar.Toggle className="!bg-red-600/50 border-2 border-red-200/50 text-white outline-none focus:ring-0 hover:!border-gray-200" />
+      <Navbar.Toggle className="!bg-red-600/50 border-2 border-red-200/50 md:hidden text-white focus:ring-0 hover:!border-gray-200" />
 
       <Navbar.Collapse>
         <li>
@@ -38,7 +35,7 @@ export default function NavbarPage() {
         <li>
           <Link
             href="/events"
-            className="block py-2 pr-4 pl-3 md:p-0 rounded-lg md:rounded-none text-white hover:bg-red-700/30 md:border-white border-red-600 hover:border-b-2 md:border-0 md:hover:bg-transparent  md:dark:hover:bg-transparent"
+            className="block py-2 pr-4 pl-3 md:p-0 rounded-lg md:rounded-none text-white hover:bg-red-700/30 md:border-white border-red-600 hover:border-b-2 md:border-0 md:hover:bg-transparent  md:dark:hover:bg-transparent "
           >
             <p>Eventos</p>
           </Link>
